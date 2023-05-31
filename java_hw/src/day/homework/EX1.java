8a8941900ea973c01e46b5b31a0423757384f009
usernameWoo <qoguddn12312@gmail.com>
2023-05-31 15:17:14 +0900

    숙제

--- java_hw/src/day/homework/EX1.java
+++ java_hw/src/day/homework/EX1.java
@@ -1,7 +1,7 @@
 package day.homework;
 
 public class EX1 {
-
+//콘솔에서 정수 2개와 문자(산술연산자)를 입력받아 출력하는 코드를 작성하세요.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- java_hw/src/day/homework/Ex2.java
+++ java_hw/src/day/homework/Ex2.java
@@ -1,10 +1,11 @@
 package day.homework;
 
 public class Ex2 {
-
+// 두 정수를 입력받아 두 정수의 산술 연산 결과를 출력하는 코드를 작성하세요. 단, 2번째 숫자는 0이 아님.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
 	}
 
 }
