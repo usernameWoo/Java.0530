fe6ca58912730b5525efeaedd62bc117966b20ec
usernameWoo <qoguddn12312@gmail.com>
2023-05-31 12:41:31 +0900

    변수 연습 문제

---
+++ java_hw/src/day2/variable/Practice.java
@@ -0,0 +1,14 @@
+package day2.variable;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		//문자는 ''로 감싸져야 한다. ""가 아님
+		char ch1 = 'A';
+
+		//과목별 성적을 입력하고 평균을 구할 때 필요한 변수 선언
+		int kor, eng, math;
+		double avg;
+	}
+
+}
